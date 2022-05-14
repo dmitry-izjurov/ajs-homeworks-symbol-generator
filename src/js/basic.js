8040d5370f471d5846d46be1236e57fc75095b42
Dmitry <dmitry.izjurov@yandex.ru>
2022-05-14 10:38:13 +0300

    Выполнено задание Символы и генераторы

--- src/js/basic.js
+++ src/js/basic.js
@@ -1,7 +1,31 @@
-export default function sum(items) {
-  let result = 0;
-  for (const item of items) {
-    result += item;
+export default class Team {
+  constructor() {
+    this.char = [
+      {
+        name: 'Зомби', type: 'Zombie', health: 50, level: 1, attack: 40, defence: 10,
+      },
+      {
+        name: 'Маг', type: 'Magican', health: 50, level: 1, attack: 40, defence: 10,
+      },
+      {
+        name: 'Лучник', type: 'Bowman', health: 50, level: 1, attack: 40, defence: 10,
+      },
+    ];
   }
-  return result;
+
+  * [Symbol.iterator]() {
+    let current = 0;
+    const last = this.char.length - 1;
+
+    while (current <= last) {
+      yield this.char[current];
+      current += 1;
+    }
+  }
+}
+
+const command = new Team();
+
+for (const unit of command) {
+  console.log(unit);
 }
